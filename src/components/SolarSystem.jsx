41b5c099d89501842dd797c5a9813b33a62ebc45
Danillo Santos <dnashy@hotmail.com>
2023-03-30 17:59:16 -0300

    Conclui requisito 2, adicionando o componente SolarSystem

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        Conteúdo
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
