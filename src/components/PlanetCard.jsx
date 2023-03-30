b57cc3e4e1d4ebd1a63f17e416d4913af11fc663
Danillo Santos <dnashy@hotmail.com>
2023-03-30 18:18:40 -0300

    Conclui requisito 5, adicionando o componente PlanetCard

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">
+          {planetName}
+        </p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string,
+  planetImage: PropTypes.string,
+};
+
+PlanetCard.defaultProps = {
+  planetName: <p>Planeta</p>,
+  planetImage: <img alt="Imagem do planeta" />,
+};
+
+export default PlanetCard;
