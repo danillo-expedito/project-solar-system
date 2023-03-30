16e4863bf7c774993dc946c5d99afcba612e6bc0
Danillo Santos <dnashy@hotmail.com>
2023-03-30 18:06:41 -0300

    Conclui requisito 3, adicionando o componente Title

---
+++ src/components/Title.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Title extends React.Component {
+  render() {
+    const { headline } = this.props;
+    return (
+      <h2>{ headline }</h2>
+    );
+  }
+}
+
+Title.propTypes = {
+  headline: PropTypes.string,
+};
+
+Title.defaultProps = {
+  headline: <h2>Sem valor</h2>,
+};
+
+export default Title;
